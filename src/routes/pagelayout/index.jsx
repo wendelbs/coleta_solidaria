ca1a320bbba746bea82643acb391d3ab8e39171d
wendelbs <wendelbs14@gmail.com>
2022-11-15 15:30:01 -0300

    Atualizando

--- src/routes/pagelayout/index.jsx
+++ src/routes/pagelayout/index.jsx
@@ -6,6 +6,7 @@ import {  NewMain, NewOutlet, TitleSite, TitleTop } from "./style";
 
 export  default function Layout() {
     return (
+  
         <NewMain>
             <TitleTop>
                 <TitleSite>Cadastro de Ponto de Entrega</TitleSite> 
